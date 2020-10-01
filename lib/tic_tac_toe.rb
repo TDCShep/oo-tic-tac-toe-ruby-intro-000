16024aeac519172bf07dc9c4af4a4f9b86513936
Tom Shephard <tom.shephard@hotmail.co.uk>
2020-10-01 12:24:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,7 +33,9 @@ class TicTacToe
         @board[index] != " " && @board[index] != ""
       end
 
-
+      def valid_move?(index)
+        index.between?(0,8) && !position_taken?(index)
+      end
 
 
 
